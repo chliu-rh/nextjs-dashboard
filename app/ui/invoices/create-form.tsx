1373b7ac30390b28cb645455a2923880f6c65bcb
Chris Liu <christopherliu@rockethomes.com>
2024-11-11 10:54:16 -0500

    form 2

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -20,7 +20,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             Choose customer
           </label>
           <div className="relative">
-            <select
+            <select 
               id="customer"
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
